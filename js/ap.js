6367cfc33511e9731b8dbeeed60f91f25d62a444
helder Malunga <helvimacuacua@gmail.com>
2023-06-11 11:08:26 -0800

    versao final

--- js/ap.js
+++ js/ap.js
@@ -1,10 +1,13 @@
 var menu=window.document.getElementById("menu");
 var menu_ul=window.document.getElementById("menu-ul");
+var img_menu=window.document.getElementById("img_menu");
 
 menu.addEventListener('click',function(){
 	if(menu_ul.classList.contains("adicionar-left")){
-       menu_ul.classList.remove("adicionar-left")
+       menu_ul.classList.remove("adicionar-left");
+       img_menu.src="icons/menu.png";
 	}else{
 		menu_ul.classList.add("adicionar-left")
+		img_menu.src="icons/closemenu.png";
 	}
 });
